1eec1ac98cae068a0477998cbd8aefe84e927566
jackie1santana <jackietristansantana@gmail.com>
2021-02-18 16:37:02 -0500

    a

---
+++ README.md
@@ -0,0 +1,4 @@
+create a weather page
+create an account to view a more detailed weather page + pollen count
+
+when signing up, check for daily newsletter
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const path = require('path')
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./model/graphqlSchema");
+const morgan = require('morgan')
+
+
+const app = express()
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+ 
+const PORT = 3000
+
+app.use(cors())
+app.use(morgan('combined'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// app.use(express.static(path.join(__dirname, 'client/dist')))
+app.get('/', (req, res) => {
+	res.send('wassuip')
+})
+
+
+
+
+
+mongoose.connect('mongodb://mongo:27017/users', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const User = mongoose.model('User', { name: String });
+
+const userOne = new User({ name: 'g' });
+userOne.save().then(() => console.log('hiiisiiiiii'));
+
+console.log('helddfdf')
+
+app.listen(PORT, () => {
+	console.log(`local server running on port ${PORT}`)
+})
